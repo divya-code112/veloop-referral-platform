41a1443a9c118b9af9e764e4ef34b7d4d2c7de34
divya-code112 <lawanddivya@gmail.com>
2026-09-23 18:45:49 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import '@fontsource-variable/sora/wght.css';
+import '@fontsource-variable/inter/wght.css';
+
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import './styles/global.css';
+
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
